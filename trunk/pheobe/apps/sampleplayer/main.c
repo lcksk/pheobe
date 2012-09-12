e70c5bc16f1ac99f0dce9e52cb5c3aff714c5f29
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2012-09-12 08:48:15 +0000

    trivial update

--- trunk/pheobe/apps/sampleplayer/main.c
+++ trunk/pheobe/apps/sampleplayer/main.c
@@ -1,19 +1,18 @@
 #include "gdbus-sampleplayer-generated.h"
+#include "def.h"
 
 static GDBusObjectManagerServer* manager = NULL;
 
-static void
-on_bus_acquired (GDBusConnection *connection,
-                 const gchar     *name,
-                 gpointer         user_data)
-{
+static void on_bus_acquired(GDBusConnection *connection, const gchar *name,
+		gpointer user_data) {
 	sampleplayerSampleplayerSkeleton *object;
-  guint n;
+	guint n;
 
-  g_print ("Acquired a message bus connection\n");
+	g_print("Acquired a message bus connection\n");
 
-  /* Create a new org.freedesktop.DBus.ObjectManager rooted at /example/Animals */
-  manager = g_dbus_object_manager_server_new ("/com/halkamalka/GDBus/PB/Sampleplayer");
+	/* Create a new org.freedesktop.DBus.ObjectManager rooted at /example/Animals */
+	manager = g_dbus_object_manager_server_new(
+			GDBUS_OBJECT_PATH);
 
 //  for (n = 0; n < 10; n++)
 //    {
@@ -57,47 +56,37 @@ on_bus_acquired (GDBusConnection *connection,
 //      g_object_unref (object);
 //    }
 
-  /* Export all objects */
-  g_dbus_object_manager_server_set_connection (manager, connection);
+	/* Export all objects */
+	g_dbus_object_manager_server_set_connection(manager, connection);
 }
 
-static void
-on_name_acquired (GDBusConnection *connection,
-                  const gchar     *name,
-                  gpointer         user_data)
-{
-  g_print ("Acquired the name %s\n", name);
+static void on_name_acquired(GDBusConnection *connection, const gchar *name,
+		gpointer user_data) {
+	g_print("Acquired the name %s\n", name);
 }
 
-static void
-on_name_lost (GDBusConnection *connection,
-              const gchar     *name,
-              gpointer         user_data)
-{
-  g_print ("Lost the name %s\n", name);
+static void on_name_lost(GDBusConnection *connection, const gchar *name,
+		gpointer user_data) {
+	g_print("Lost the name %s\n", name);
 }
 
 gint main(gint argc, gchar* argv[]) {
 
-		GMainLoop* loop;
-		guint id;
+	GMainLoop* loop;
+	guint id;
 
-		g_type_init();
+	g_type_init();
 
-		loop = g_main_loop_new (NULL, FALSE);
-		id = g_bus_own_name (G_BUS_TYPE_SESSION,
-														"com.halkamalka.GDBus.PB.Sampleplayer.ObjectManager",
-														G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT | G_BUS_NAME_OWNER_FLAGS_REPLACE,
-														on_bus_acquired,
-														on_name_acquired,
-														on_name_lost,
-														loop,
-														NULL
-			);
+	loop = g_main_loop_new(NULL, FALSE);
+	id = g_bus_own_name(G_BUS_TYPE_SESSION,
+			GDBUS_NAME,
+			G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT
+					| G_BUS_NAME_OWNER_FLAGS_REPLACE, on_bus_acquired,
+			on_name_acquired, on_name_lost, loop, NULL);
 
-		g_main_loop_run(loop);
-		g_bus_unown_name(id);
-		g_main_loop_unref(loop);
+	g_main_loop_run(loop);
+	g_bus_unown_name(id);
+	g_main_loop_unref(loop);
 
-		return 0;
+	return 0;
 }
