22a9987bfeed6480588d645a7669232e30876345
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2012-09-12 11:48:41 +0000

    step by step

--- trunk/pheobe/apps/Renderer/main.c
+++ trunk/pheobe/apps/Renderer/main.c
@@ -5,167 +5,133 @@
  *      Author: buttonfly
  */
 
-
-
-
 #include "gdbus-Renderer-generated.h"
+#include "def-renderer.h"
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 static GDBusObjectManagerServer *manager = NULL;
 
-static gboolean
-on_animal_poke (PheobeAnimal          *animal,
-                GDBusMethodInvocation  *invocation,
-                gboolean                make_sad,
-                gboolean                make_happy,
-                gpointer                user_data)
-{
-  if ((make_sad && make_happy) || (!make_sad && !make_happy))
-    {
-      g_dbus_method_invocation_return_dbus_error (invocation,
-                                                  "com.halkamalka.GDBus.Pheobe.ObjectManager.Error.Failed",
-                                                  "Exactly one of make_sad or make_happy must be TRUE");
-      goto out;
-    }
+static gboolean on_renderer_poke(PheobeRenderer *renderer,
+		GDBusMethodInvocation *invocation, gboolean make_sad,
+		gboolean make_happy, gpointer user_data) {
+	if ((make_sad && make_happy) || (!make_sad && !make_happy)) {
+		g_dbus_method_invocation_return_dbus_error(invocation,
+				GDBUS_RENDERER_INTERFACE_NAME".Error.Failed",
+				"Exactly one of make_sad or make_happy must be TRUE");
+		goto out;
+	}
 
-  if (make_sad)
-    {
-      if (g_strcmp0 (pheobe_animal_get_mood (animal), "Sad") == 0)
-        {
-          g_dbus_method_invocation_return_dbus_error (invocation,
-                                                      "com.halkamalka.GDBus.Pheobe.ObjectManager.Error.SadAnimalIsSad",
-                                                      "Sad animal is already sad");
-          goto out;
-        }
+	if (make_sad) {
+		if (g_strcmp0(pheobe_renderer_get_mood(renderer), "Sad") == 0) {
+			g_dbus_method_invocation_return_dbus_error(invocation,
+					GDBUS_RENDERER_INTERFACE_NAME".Error.SadAnimalIsSad",
+					"Sad animal is already sad");
+			goto out;
+		}
 
-      pheobe_animal_set_mood (animal, "Sad");
-      pheobe_animal_complete_poke (animal, invocation);
-      goto out;
-    }
+		pheobe_renderer_set_mood(renderer, "Sad");
+		pheobe_renderer_complete_poke(renderer, invocation);
+		goto out;
+	}
 
-  if (make_happy)
-    {
-      if (g_strcmp0 (pheobe_animal_get_mood (animal), "Happy") == 0)
-        {
-          g_dbus_method_invocation_return_dbus_error (invocation,
-                                                      "com.halkamalka.GDBus.Pheobe.ObjectManager.Error.HappyAnimalIsHappy",
-                                                      "Happy animal is already happy");
-          goto out;
-        }
+	if (make_happy) {
+		if (g_strcmp0(pheobe_renderer_get_mood(renderer), "Happy") == 0) {
+			g_dbus_method_invocation_return_dbus_error(invocation,
+					GDBUS_RENDERER_INTERFACE_NAME".Error.HappyAnimalIsHappy",
+					"Happy animal is already happy");
+			goto out;
+		}
 
-      pheobe_animal_set_mood (animal, "Happy");
-      pheobe_animal_complete_poke (animal, invocation);
-      goto out;
-    }
+		pheobe_renderer_set_mood(renderer, "Happy");
+		pheobe_renderer_complete_poke(renderer, invocation);
+		goto out;
+	}
 
-  g_assert_not_reached ();
+	g_assert_not_reached();
 
- out:
-  return TRUE; /* to indicate that the method was handled */
+	out: return TRUE; /* to indicate that the method was handled */
 }
 
+static void on_bus_acquired(GDBusConnection *connection, const gchar *name,
+		gpointer user_data) {
+	PheobeObjectSkeleton *object;
+	guint n;
 
-static void
-on_bus_acquired (GDBusConnection *connection,
-                 const gchar     *name,
-                 gpointer         user_data)
-{
-  PheobeObjectSkeleton *object;
-  guint n;
+	g_print("Acquired a message bus connection\n");
 
-  g_print ("Acquired a message bus connection\n");
+	/* Create a new org.freedesktop.DBus.ObjectManager rooted at /example/renderers */
+	manager = g_dbus_object_manager_server_new(GDBUS_RENDERER_OBJECT_PATH);
 
-  /* Create a new org.freedesktop.DBus.ObjectManager rooted at /example/Animals */
-  manager = g_dbus_object_manager_server_new ("/pheobe/Animals");
+	for (n = 0; n < 10; n++) {
+		gchar *s;
+		PheobeRenderer *renderer;
 
-  for (n = 0; n < 10; n++)
-    {
-      gchar *s;
-      PheobeAnimal *animal;
+		/* Create a new D-Bus object at the path /example/renderers/N where N is 000..009 */
+		s = g_strdup_printf(GDBUS_RENDERER_OBJECT_PATH"/%03d", n);
+		object = pheobe_object_skeleton_new(s);
+		g_free(s);
 
-      /* Create a new D-Bus object at the path /example/Animals/N where N is 000..009 */
-      s = g_strdup_printf ("/pheobe/Animals/%03d", n);
-      object = pheobe_object_skeleton_new (s);
-      g_free (s);
+		/* Make the newly created object export the interface
+		 * org.gtk.GDBus.Example.ObjectManager.renderer (note
+		 * that @object takes its own reference to @animal).
+		 */
+		renderer = pheobe_renderer_skeleton_new();
+		pheobe_renderer_set_mood(renderer, "Happy");
+		pheobe_object_skeleton_set_renderer(object, renderer);
+		g_object_unref(renderer);
 
-      /* Make the newly created object export the interface
-       * org.gtk.GDBus.Example.ObjectManager.Animal (note
-       * that @object takes its own reference to @animal).
-       */
-      animal = pheobe_animal_skeleton_new ();
-      pheobe_animal_set_mood (animal, "Happy");
-      pheobe_object_skeleton_set_animal (object, animal);
-      g_object_unref (animal);
+		/* Cats are odd animals - so some of our objects implement the
+		 * org.gtk.GDBus.Example.ObjectManager.Cat interface in addition
+		 * to the .Animal interface
+		 */
+		if (n % 2 == 1) {
+			PheobeCat *cat;
+			cat = pheobe_cat_skeleton_new();
+			pheobe_object_skeleton_set_cat(object, cat);
+			g_object_unref(cat);
+		}
 
-      /* Cats are odd animals - so some of our objects implement the
-       * org.gtk.GDBus.Example.ObjectManager.Cat interface in addition
-       * to the .Animal interface
-       */
-      if (n % 2 == 1)
-        {
-          PheobeCat *cat;
-          cat = pheobe_cat_skeleton_new ();
-          pheobe_object_skeleton_set_cat (object, cat);
-          g_object_unref (cat);
-        }
+		/* Handle Poke() D-Bus method invocations on the .renderer interface */
+		g_signal_connect(renderer, "handle-poke", G_CALLBACK(on_renderer_poke),
+				NULL); /* user_data */
 
-      /* Handle Poke() D-Bus method invocations on the .Animal interface */
-      g_signal_connect (animal,
-                        "handle-poke",
-                        G_CALLBACK (on_animal_poke),
-                        NULL); /* user_data */
+		/* Export the object (@manager takes its own reference to @object) */
+		g_dbus_object_manager_server_export(manager,
+				G_DBUS_OBJECT_SKELETON(object));
+		g_object_unref(object);
+	}
 
-      /* Export the object (@manager takes its own reference to @object) */
-      g_dbus_object_manager_server_export (manager, G_DBUS_OBJECT_SKELETON (object));
-      g_object_unref (object);
-    }
-
-  /* Export all objects */
-  g_dbus_object_manager_server_set_connection (manager, connection);
+	/* Export all objects */
+	g_dbus_object_manager_server_set_connection(manager, connection);
 }
 
-static void
-on_name_acquired (GDBusConnection *connection,
-                  const gchar     *name,
-                  gpointer         user_data)
-{
-  g_print ("Acquired the name %s\n", name);
+static void on_name_acquired(GDBusConnection *connection, const gchar *name,
+		gpointer user_data) {
+	g_print("Acquired the name %s\n", name);
 }
 
-static void
-on_name_lost (GDBusConnection *connection,
-              const gchar     *name,
-              gpointer         user_data)
-{
-  g_print ("Lost the name %s\n", name);
+static void on_name_lost(GDBusConnection *connection, const gchar *name,
+		gpointer user_data) {
+	g_print("Lost the name %s\n", name);
 }
 
+gint main(gint argc, gchar *argv[]) {
+	GMainLoop *loop;
+	guint id;
 
-gint
-main (gint argc, gchar *argv[])
-{
-  GMainLoop *loop;
-  guint id;
+	g_type_init();
 
-  g_type_init ();
+	loop = g_main_loop_new(NULL, FALSE);
+	id = g_bus_own_name(G_BUS_TYPE_SESSION, GDBUS_RENDERER_INTERFACE_NAME,
+			G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT
+					| G_BUS_NAME_OWNER_FLAGS_REPLACE, on_bus_acquired,
+			on_name_acquired, on_name_lost, loop, NULL);
 
-  loop = g_main_loop_new (NULL, FALSE);
+	g_main_loop_run(loop);
 
-  id = g_bus_own_name (G_BUS_TYPE_SESSION,
-                       "com.halkamalka.GDBus.Pheobe.ObjectManager",
-                       G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
-                       G_BUS_NAME_OWNER_FLAGS_REPLACE,
-                       on_bus_acquired,
-                       on_name_acquired,
-                       on_name_lost,
-                       loop,
-                       NULL);
+	g_bus_unown_name(id);
+	g_main_loop_unref(loop);
 
-  g_main_loop_run (loop);
-
-  g_bus_unown_name (id);
-  g_main_loop_unref (loop);
-
-  return 0;
+	return 0;
 }
