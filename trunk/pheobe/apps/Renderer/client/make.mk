9aa53b127e8406279e97b3645163b22fbfdf7257
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2012-09-12 11:08:09 +0000

    gdbus: basic example.

---
+++ trunk/pheobe/apps/Renderer/client/make.mk
@@ -0,0 +1,21 @@
+include $(BUILD_ROOT)/tools/config.mk
+include $(BUILD_ROOT)/tools/library.mk
+
+INCLUDE_DIR = \
+	../include	\
+	../gdbus
+
+CFLAGS += $(addprefix -I, $(INCLUDE_DIR))
+
+CFLAGS += `pkg-config --cflags gstreamer-0.10`
+CFLAGS += `pkg-config --cflags gio-unix-2.0`
+
+LDFLAGS += `pkg-config --libs gstreamer-0.10`
+LDFLAGS += `pkg-config --libs gio-unix-2.0`
+
+vpath %.c ../gdbus
+
+OBJS += \
+	main.o	\
+	gdbus-Renderer-generated.o
+	
