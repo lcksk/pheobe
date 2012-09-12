6f7af53ba4194535d11eeb7a0a33a72c6d118594
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2012-09-12 03:45:43 +0000

    wrong import

--- trunk/pheobe/pheobe/apps/sampleplayer/make.mk
+++
@@ -1,9 +0,0 @@
-include $(BUILD_ROOT)/tools/config.mk
-include $(BUILD_ROOT)/tools/library.mk
-
-CFLAGS += `pkg-config --cflags gstreamer-0.10`
-LDFLAGS += `pkg-config --libs gstreamer-0.10`
-
-OBJS += \
-	main.o	\
-
