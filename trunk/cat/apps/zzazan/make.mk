46b5f963657016f324bb63f0a85b35ab59f583ee
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2012-09-20 15:38:32 +0000

    rename

---
+++ trunk/cat/apps/zzazan/make.mk
@@ -0,0 +1,39 @@
+include $(BUILD_ROOT)/tools/config.mk
+include $(BUILD_ROOT)/tools/library.mk
+
+INCLUDE_DIR = \
+	include 	\
+	$(APPS_DIR)/framework	\
+	$(APPS_DIR)/framework/service	\
+	$(APP_DIR)/service/playback	\
+	$(APP_DIR)/service/upnp	\
+	$(APPS_DIR)/platform	\
+
+CFLAGS += $(addprefix -I, $(INCLUDE_DIR))
+
+CFLAGS += `pkg-config clutter-1.0 --cflags`
+
+#CFLAGS += `pkg-config --cflags gstreamer-0.10`
+#CFLAGS += `pkg-config --cflags gio-unix-2.0`
+CFLAGS += `pkg-config --cflags clutter-gst-1.0`
+
+LDFLAGS += `pkg-config clutter-1.0 --libs`
+ 
+#LDFLAGS += `pkg-config --libs gstreamer-0.10`
+#LDFLAGS += `pkg-config --libs gio-unix-2.0`
+LDFLAGS += `pkg-config --libs clutter-gst-1.0`
+
+LDFLAGS += -L$(APPS_DIR)/$(ARCH).$(DEBUG_SUFFIX)/lib -lframework -lplatform
+
+
+vpath %.cpp %.h  $(APP_DIR)/service/playback
+vpath %.cpp %.h  $(APP_DIR)/service/view
+vpath %.cpp %.h  $(APP_DIR)/service/upnp
+
+include $(APP_DIR)/service/playback/make.mk
+include $(APP_DIR)/service/view/make.mk
+include $(APP_DIR)/service/upnp/make.mk
+
+OBJS += \
+	main.o	\
+
