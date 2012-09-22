1dc296c22524c01f9170a6bea0c4c021e0eb755e
buttonfly <buttonfly@gmail.com>
2012-09-22 09:55:49 +0000

    daily update

--- cat/apps/catorendo/upnp/IUpnpDeviceListener.h
+++ cat/apps/catorendo/upnp/IUpnpDeviceListener.h
@@ -9,14 +9,21 @@
 #define IUPNPDEVICELISTENER_H_
 
 #include <libgupnp/gupnp-control-point.h>
-#include <libgupnp-dlna/gupnp-dlna-discoverer.h>
+#include <libgupnp-av/gupnp-av.h>
+//#include <libgupnp-dlna/gupnp-dlna-discoverer.h>
 
 namespace halkamalka {
 
 class IUpnpDeviceListener {
 public:
-	virtual void deviceAdded(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)=0;
-	virtual void deviceRemoved(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)=0;
+	virtual void deviceAdded(GUPnPControlPoint *cp, GUPnPDeviceProxy  *proxy)=0;
+	virtual void deviceRemoved(GUPnPControlPoint *cp, GUPnPDeviceProxy  *proxy)=0;
+
+#if 0
+	virtual void serviceAdded(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)=0;
+	virtual void serviceRemoved(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)=0;
+#endif
+	virtual void didlObjectFound(GUPnPDIDLLiteObject* object)=0;
 };
 
 } /* namespace halkamalka */
--- cat/apps/catorendo/view/subNoti.cpp
+++ cat/apps/catorendo/view/subNoti.cpp
@@ -8,6 +8,8 @@
 #include "subNoti.h"
 #include "UpnpControlPoint.h"
 
+#include "dbg.h"
+
 namespace halkamalka {
 
 subNoti::subNoti()
@@ -22,7 +24,6 @@ subNoti::~subNoti() {
 
 void subNoti::createPartControl(ClutterActor* stage)
 {
-
 	m_text = clutter_text_new ();
 	ClutterColor color = { 0x60, 0x60, 0x90, 0xFF }; /* blueish */
 	clutter_text_set_color (CLUTTER_TEXT (m_text), &color);
@@ -45,15 +46,65 @@ void subNoti::setVisible(bool visible)
 
 void subNoti::fadeEffectCompleted(ClutterTimeline* timeline, gpointer data)
 {
+	S_;
 	subNoti* pThis = static_cast<subNoti*>(data);
-	g_print ("fadeEffectCompleted\n");
 	g_object_unref(pThis->m_behaviourOpacity);
 	pThis->m_behaviourOpacity = NULL;
 }
 
-void subNoti::deviceAdded(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)
+#if 0
+void subNoti::serviceAdded(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)
 {
-	g_print ("deviceAdded\n");
+	S_;
+	if(m_fadeEffectTimeline &&  clutter_timeline_is_playing (m_fadeEffectTimeline) ) {
+		return;
+	}
+	clutter_timeline_stop(m_fadeEffectTimeline);
+	ClutterAlpha *alpha = clutter_alpha_new_full (m_fadeEffectTimeline, CLUTTER_EASE_IN_SINE);
+	m_behaviourOpacity = clutter_behaviour_opacity_new (alpha, 0, 255);
+	clutter_behaviour_apply (m_behaviourOpacity, m_text);
+	const char* udn = gupnp_service_info_get_udn(GUPNP_SERVICE_INFO(proxy));
+	clutter_text_set_text (CLUTTER_TEXT (m_text), udn);
+	clutter_timeline_start(m_fadeEffectTimeline);
+}
+
+void subNoti::serviceRemoved(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)
+{
+	S_;
+#if 0
+	if(m_fadeEffectTimeline &&  clutter_timeline_is_playing (m_fadeEffectTimeline) ) {
+		return;
+	}
+	clutter_timeline_stop(m_fadeEffectTimeline);
+	ClutterAlpha *alpha = clutter_alpha_new_full (m_fadeEffectTimeline, CLUTTER_EASE_IN_SINE);
+	m_behaviourOpacity = clutter_behaviour_opacity_new (alpha, (guint8)0,  (guint8)255);
+	clutter_behaviour_apply (m_behaviourOpacity, m_text);
+	const char* udn = gupnp_service_info_get_udn(GUPNP_SERVICE_INFO(proxy));
+	clutter_text_set_text (CLUTTER_TEXT (m_text), udn);
+	clutter_timeline_start(m_fadeEffectTimeline);
+#endif
+}
+#endif
+void subNoti::didlObjectFound(GUPnPDIDLLiteObject* object)
+{
+	S_;
+	if(m_fadeEffectTimeline &&  clutter_timeline_is_playing (m_fadeEffectTimeline) ) {
+		return;
+	}
+
+	clutter_timeline_stop(m_fadeEffectTimeline);
+	ClutterAlpha *alpha = clutter_alpha_new_full (m_fadeEffectTimeline, CLUTTER_EASE_IN_SINE);
+//	m_behaviourOpacity = clutter_behaviour_opacity_new (alpha, (guint8)0,  (guint8)255);
+
+	clutter_behaviour_apply (m_behaviourOpacity, m_text);
+	const char* title  = gupnp_didl_lite_object_get_title(object);
+	clutter_text_set_text (CLUTTER_TEXT (m_text), title);
+	clutter_timeline_start(m_fadeEffectTimeline);
+}
+
+void subNoti::deviceAdded(GUPnPControlPoint *cp, GUPnPDeviceProxy  *proxy)
+{
+	S_;
 	clutter_timeline_stop(m_fadeEffectTimeline);
 	ClutterAlpha *alpha = clutter_alpha_new_full (m_fadeEffectTimeline, CLUTTER_EASE_IN_SINE);
 	m_behaviourOpacity = clutter_behaviour_opacity_new (alpha, (guint8)0,  (guint8)255);
@@ -61,11 +112,20 @@ void subNoti::deviceAdded(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)
 	const char* friendlyName = gupnp_device_info_get_friendly_name(GUPNP_DEVICE_INFO(proxy));
 	clutter_text_set_text (CLUTTER_TEXT (m_text), friendlyName);
 	clutter_timeline_start(m_fadeEffectTimeline);
+
+
+	//TODO: test
+//	GUPnPServiceInfo*  service = gupnp_device_info_get_service(GUPNP_DEVICE_INFO(proxy), "urn:schemas-upnp-org:service:ContentDirectory:1");
+//
+//	if(service) {
+//		g_print("service is not null!!!!!!!!!!!!!!!!!!\n");
+//		g_object_unref(service);
+//	}
 }
 
-void subNoti::deviceRemoved(GUPnPControlPoint *cp, GUPnPServiceProxy *proxy)
+void subNoti::deviceRemoved(GUPnPControlPoint *cp, GUPnPDeviceProxy  *proxy)
 {
-	g_print ("deviceRemoved\n");
+	S_;
 	clutter_timeline_stop(m_fadeEffectTimeline);
 	ClutterAlpha *alpha = clutter_alpha_new_full (m_fadeEffectTimeline, CLUTTER_EASE_IN_SINE);
 	m_behaviourOpacity = clutter_behaviour_opacity_new (alpha, 0, 255);
