51e32b1047c46680a5792ca64f5be917f7b00c3f
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2013-01-03 17:01:08 +0000

    add env for 7358

---
+++ trunk/scriptkid/env/env-97358a0.sh
@@ -0,0 +1,14 @@
+export PLATFORM=97358
+export BCHP_VER=A0
+export BCHP_CHIP=7358
+
+export ARCH=mipsel-linux
+
+export USERNAME=$USER
+
+export LINUX=/home/$USERNAME/workspace/olleh/stblinux-2.6.37
+export ROOTFS=/home/$USERNAME/workspace/olleh/uclinux-rootfs
+export TOOLCHAIN_DIR=/home/$USERNAME/workspace/toolchain/stbgcc-4.5.3-1.3
+export PATH=$TOOLCHAIN_DIR/bin:$PATH
+
+
