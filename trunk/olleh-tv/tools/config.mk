a7f22c615066cc17345d6d9814d1603dd6d0ae73
buttonfly <buttonfly@gmail.com>
2013-04-17 02:14:49 +0000

    update buildtree for opensource

--- trunk/olleh-tv/tools/config.mk
+++ trunk/olleh-tv/tools/config.mk
@@ -4,6 +4,9 @@ BUILD_ROOT ?= ..
 export BIN_DIR = $(BUILD_ROOT)/bin
 APPS_DIR = $(BUILD_ROOT)/apps
 
+OPENSOURCE_DIR = $(BUILD_ROOT)/opensource
+OPENSOURCE_PREFIX = $(OPENSOURCE_DIR)/$(ARCH)
+
 ifeq ($(DEBUG),n)
 OBJ_DIR:= $(BUILD_ROOT)/release
 else
