9c638b839300dbde92d3d4d95dbf8f47752879b2
buttonfly@gmail.com <buttonfly@gmail.com@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2013-04-18 06:43:45 +0000

    add PKG_CONFIG_PATH

--- trunk/trunk/olleh-tv/tools/config.mk
+++ trunk/trunk/olleh-tv/tools/config.mk
@@ -7,6 +7,8 @@ APPS_DIR = $(BUILD_ROOT)/apps
 OPENSOURCE_DIR = $(BUILD_ROOT)/opensource
 OPENSOURCE_PREFIX = $(OPENSOURCE_DIR)/$(ARCH)
 
+export PKG_CONFIG_PATH = $(OPENSOURCE_PREFIX)/usr/local/lib/pkgconfig
+
 ifeq ($(DEBUG),n)
 OBJ_DIR:= $(BUILD_ROOT)/release
 else
