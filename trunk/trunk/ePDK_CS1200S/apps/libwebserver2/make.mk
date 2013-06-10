cbcfca9646b435534647b1ca7e91f7880c95c8a6
buttonfly <buttonfly@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2013-06-10 17:43:11 +0000

    downloaded the lastest mongoose c++.

---
+++ trunk/trunk/ePDK_CS1200S/apps/libwebserver2/make.mk
@@ -0,0 +1,19 @@
+include $(BUILD_ROOT)/tools/library.mk
+
+INCLUDE_DIR = \
+			include	\
+			$(APPS_DIR)/libosl/include
+
+
+#CFLAGS :=      $(filter-out -std=c89, $(CFLAGS))
+#CFLAGS :=      $(filter-out -Wstrict-prototypes, $(CFLAGS))
+
+CFLAGS += $(addprefix -I, $(INCLUDE_DIR))
+CFLAGS += -DREENTRANT $(addprefix -I, $(INCLUDE_DIR))
+CFLAGS += -pthread -DUSE_WEBSOCKET
+LDFLAGS += -L$(BUILD_ROOT)/bin -lpthread
+
+OBJS += \
+	mongoose.o	\
+	mongcpp.o
+
