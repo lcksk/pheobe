7db357e64c5d54fa2a7472c62025ff56931b7a0c
buttonfly <buttonfly@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2013-06-11 02:37:14 +0000

    add json++ and jsonrpc++

--- trunk/trunk/ePDK_CS1200S/apps/libwebserver/make.mk
+++ trunk/trunk/ePDK_CS1200S/apps/libwebserver/make.mk
@@ -13,6 +13,13 @@ CFLAGS += -DREENTRANT $(addprefix -I, $(INCLUDE_DIR))
 CFLAGS += -pthread -DUSE_WEBSOCKET
 LDFLAGS += -L$(BUILD_ROOT)/bin -lpthread
 
+
+vpath %.cpp %.h  json++
+include  json++/make.mk
+
+vpath %.cpp %.h  jsonrpc++
+include  jsonrpc++/make.mk
+
 OBJS += \
 	mongoose.o	\
 	webservice.o	\
