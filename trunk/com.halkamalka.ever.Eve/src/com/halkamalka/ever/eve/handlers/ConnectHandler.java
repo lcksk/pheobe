8fc39858ad9f99ea6d44d13da7d7c8e13f1a99f4
buttonfly <buttonfly@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2014-05-12 04:07:26 +0000

    adding menu : connect, list product

---
+++ trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/handlers/ConnectHandler.java
@@ -0,0 +1,17 @@
+package com.halkamalka.ever.eve.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class ConnectHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/handlers/ListProductHandler.java
@@ -0,0 +1,18 @@
+package com.halkamalka.ever.eve.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class ListProductHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
