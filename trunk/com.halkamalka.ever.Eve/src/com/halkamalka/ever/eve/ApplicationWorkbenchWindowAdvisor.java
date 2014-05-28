2b21700a5e801cf2abeefee1ac453808034d7f49
buttonfly <buttonfly@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2014-05-28 02:26:46 +0000

    Update Site (not finished yet)

--- trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/ApplicationWorkbenchWindowAdvisor.java
+++ trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/ApplicationWorkbenchWindowAdvisor.java
@@ -2,12 +2,17 @@ package com.halkamalka.ever.eve;
 
 import java.io.IOException;
 
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 import com.halkamalka.ever.eve.core.data.DataManager;
+import com.halkamalka.util.P2Util;
+import com.halkamalka.util.UpdateJob;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -27,6 +32,26 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
         configurer.setShowStatusLine(true);
         configurer.setShowProgressIndicator(true);
         configurer.setTitle("EVER&EVER - http://www.evernever.co.kr"); //$NON-NLS-1$
+    }
+    
+    
+    @Override
+    public void postWindowOpen() {
+        BundleContext bundleContext = Activator.getDefault().getBundle().getBundleContext();
+        ServiceReference<IProvisioningAgent> serviceReference =   bundleContext.getServiceReference(IProvisioningAgent.class);
+        IProvisioningAgent agent = bundleContext.getService(serviceReference);
+        if (agent == null) {
+            System.out.println(">> no agent loaded!");
+            return;
+        }
+        // Adding the repositories to explore
+        if (! P2Util.addRepository(agent, "http://halkamalka.com/p2/eve/repository")) {
+            System.out.println(">> could no add repostory!");
+            return;
+        }
+        // scheduling job for updates
+        UpdateJob updateJob = new UpdateJob(agent);
+        updateJob.schedule();
     }
     
     @Override
