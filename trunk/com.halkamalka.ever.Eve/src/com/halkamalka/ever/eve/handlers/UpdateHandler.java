3e95917db4e75b733329ea88a3248482e9269009
buttonfly <buttonfly@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2014-05-30 03:42:24 +0000

    download URL instead of websocket

--- trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/handlers/UpdateHandler.java
+++ trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/handlers/UpdateHandler.java
@@ -1,198 +1,164 @@
-package com.halkamalka.ever.eve.handlers;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.inject.Inject;
-
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.e4.ui.di.UISynchronize;
-import org.eclipse.equinox.p2.core.IProvisioningAgent;
-import org.eclipse.equinox.p2.operations.ProvisioningJob;
-import org.eclipse.equinox.p2.operations.ProvisioningSession;
-import org.eclipse.equinox.p2.operations.Update;
-import org.eclipse.equinox.p2.operations.UpdateOperation;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbench;
-
-// 
-// Require-Bundle: org.eclipse.equinox.p2.core|engine|operation|metadata.repository
-// Feature: org.eclipse.equinox.p2.core.feature
+//package com.halkamalka.ever.eve.handlers;
 //
-// !!!!!!!!!!!!!!!!!
-// do not run the update from for IDE. Update only works in an exported application 
-// !!!!!!!!!!!!!!!!!
-// 
-
-public class UpdateHandler extends AbstractHandler{
-  // repository location needs to be adjusted for your 
-  // location
-  private static final String REPOSITORY_LOC = 
-      System.getProperty("UpdateHandler.Repo", 
-      "file://home/vogella/repository");
-
-//get UISynchronize injected as field
-  @Inject
-  UISynchronize sync;
-
-  public void execute(final IProvisioningAgent agent, final Shell parent,
-      final UISynchronize sync, final IWorkbench workbench) {
-    Job j = new Job("Update Job") {
-      private boolean doInstall = false;
-
-      @Override
-      protected IStatus run(final IProgressMonitor monitor) {
-
-        /* 1. Prepare update plumbing */
-
-        final ProvisioningSession session = new ProvisioningSession(agent);
-        final UpdateOperation operation = new UpdateOperation(session);
-
-        // create uri
-        URI uri = null;
-        try {
-          uri = new URI(REPOSITORY_LOC);
-        } catch (final URISyntaxException e) {
-          sync.syncExec(new Runnable() {
-            @Override
-            public void run() {
-              MessageDialog
-              .openError(parent, "URI invalid", e.getMessage());
-            }
-          });
-          return Status.CANCEL_STATUS;
-        }
-
-        // set location of artifact and metadata repo
-        operation.getProvisioningContext().setArtifactRepositories(new URI[] { uri });
-        operation.getProvisioningContext().setMetadataRepositories(new URI[] { uri });
-
-        /* 2. check for updates */
-
-        // run update checks causing I/O
-        final IStatus status = operation.resolveModal(monitor);
-
-        // failed to find updates (inform user and exit)
-        if (status.getCode() == UpdateOperation.STATUS_NOTHING_TO_UPDATE) {
-          sync.syncExec(new Runnable() {
-            @Override
-            public void run() {
-              MessageDialog
-                  .openWarning(parent, "No update",
-                      "No updates for the current installation have been found");
-            }
-          });
-          return Status.CANCEL_STATUS;
-        }
-
-        /* 3. Ask if updates should be installed and run installation */
-
-        // found updates, ask user if to install?
-        if (status.isOK() && status.getSeverity() != IStatus.ERROR) {
-          sync.syncExec(new Runnable() {
-            @Override
-            public void run() {
-              String updates = "";
-              Update[] possibleUpdates = operation
-                  .getPossibleUpdates();
-              for (Update update : possibleUpdates) {
-                updates += update + "\n";
-              }
-              doInstall = MessageDialog.openQuestion(parent,
-                  "Really install updates?", updates);
-            }
-          });
-        }
-
-        // start installation
-        if (doInstall) {
-          final ProvisioningJob provisioningJob = operation
-              .getProvisioningJob(monitor);
-          // updates cannot run from within Eclipse IDE!!!
-          if (provisioningJob == null) {
-            System.err
-                .println("Running update from within Eclipse IDE? This won't work!!!");
-            throw new NullPointerException();
-          }
-
-          // register a job change listener to track
-          // installation progress and notify user upon success
-          provisioningJob
-              .addJobChangeListener(new JobChangeAdapter() {
-                @Override
-                public void done(IJobChangeEvent event) {
-                  if (event.getResult().isOK()) {
-                    sync.syncExec(new Runnable() {
-
-                      @Override
-                      public void run() {
-                        boolean restart = MessageDialog
-                            .openQuestion(parent,
-                                "Updates installed, restart?",
-                                "Updates have been installed successfully, do you want to restart?");
-                        if (restart) {
-                          workbench.restart();
-                        }
-                      }
-                    });
-
-                  }
-                  super.done(event);
-                }
-              });
-
-          provisioningJob.schedule();
-        }
-        return Status.OK_STATUS;
-      }
-    };
-    j.schedule();
-  }
-
-@Override
-public void addHandlerListener(IHandlerListener handlerListener) {
-	// TODO Auto-generated method stub
-	
-}
-
-@Override
-public void dispose() {
-	// TODO Auto-generated method stub
-	
-}
-
-@Override
-public Object execute(ExecutionEvent event) throws ExecutionException {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public boolean isEnabled() {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public boolean isHandled() {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public void removeHandlerListener(IHandlerListener handlerListener) {
-	// TODO Auto-generated method stub
-	
-}
-} 
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import java.util.logging.Logger;
+//
+//import org.eclipse.core.runtime.IProgressMonitor;
+//import org.eclipse.core.runtime.IStatus;
+//import org.eclipse.core.runtime.Status;
+//import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+//import org.eclipse.core.runtime.jobs.Job;
+//import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+//import org.eclipse.e4.ui.di.UISynchronize;
+//import org.eclipse.equinox.p2.core.IProvisioningAgent;
+//import org.eclipse.equinox.p2.operations.ProvisioningJob;
+//import org.eclipse.equinox.p2.operations.ProvisioningSession;
+//import org.eclipse.equinox.p2.operations.Update;
+//import org.eclipse.equinox.p2.operations.UpdateOperation;
+//import org.eclipse.jface.dialogs.MessageDialog;
+//import org.eclipse.swt.widgets.Shell;
+//import org.eclipse.ui.IWorkbench;
+//
+//// 
+//// Require-Bundle: org.eclipse.equinox.p2.core|engine|operation|metadata.repository
+//// Feature: org.eclipse.equinox.p2.core.feature
+////
+//// !!!!!!!!!!!!!!!!!
+//// do not run the update from for IDE. Update only works in an exported application 
+//// !!!!!!!!!!!!!!!!!
+//// 
+//
+//public class UpdateHandler{
+//	
+//	final private static Logger log = Logger.getLogger(UpdateHandler.class.getName());
+//	
+//  // repository location needs to be adjusted for your 
+//  // location
+//  private static final String REPOSITORY_LOC = 
+//      System.getProperty("UpdateHandler.Repo", 
+//      "http://halkamalka.com/p2/eve/repository");
+//
+////get UISynchronize injected as field
+////  @Inject
+////  UISynchronize sync;
+//
+//  @Execute
+//  public void execute(final IProvisioningAgent agent, final Shell parent,
+//      final UISynchronize sync, final IWorkbench workbench) {
+//	  
+//	  log.info("");
+//    
+//	  Job j = new Job("Update Job") {
+//      private boolean doInstall = false;
+//
+//      @Override
+//      protected IStatus run(final IProgressMonitor monitor) {
+//
+//        /* 1. Prepare update plumbing */
+//
+//        final ProvisioningSession session = new ProvisioningSession(agent);
+//        final UpdateOperation operation = new UpdateOperation(session);
+//
+//        // create uri
+//        URI uri = null;
+//        try {
+//          uri = new URI(REPOSITORY_LOC);
+//        } catch (final URISyntaxException e) {
+//          sync.syncExec(new Runnable() {
+//            @Override
+//            public void run() {
+//              MessageDialog
+//              .openError(parent, "URI invalid", e.getMessage());
+//            }
+//          });
+//          return Status.CANCEL_STATUS;
+//        }
+//
+//        // set location of artifact and metadata repo
+//        operation.getProvisioningContext().setArtifactRepositories(new URI[] { uri });
+//        operation.getProvisioningContext().setMetadataRepositories(new URI[] { uri });
+//
+//        /* 2. check for updates */
+//
+//        // run update checks causing I/O
+//        final IStatus status = operation.resolveModal(monitor);
+//
+//        // failed to find updates (inform user and exit)
+//        if (status.getCode() == UpdateOperation.STATUS_NOTHING_TO_UPDATE) {
+//          sync.syncExec(new Runnable() {
+//            @Override
+//            public void run() {
+//              MessageDialog
+//                  .openWarning(parent, "No update",
+//                      "No updates for the current installation have been found");
+//            }
+//          });
+//          return Status.CANCEL_STATUS;
+//        }
+//
+//        /* 3. Ask if updates should be installed and run installation */
+//
+//        // found updates, ask user if to install?
+//        if (status.isOK() && status.getSeverity() != IStatus.ERROR) {
+//          sync.syncExec(new Runnable() {
+//            @Override
+//            public void run() {
+//              String updates = "";
+//              Update[] possibleUpdates = operation
+//                  .getPossibleUpdates();
+//              for (Update update : possibleUpdates) {
+//                updates += update + "\n";
+//              }
+//              doInstall = MessageDialog.openQuestion(parent,
+//                  "Really install updates?", updates);
+//            }
+//          });
+//        }
+//
+//        // start installation
+//        if (doInstall) {
+//          final ProvisioningJob provisioningJob = operation
+//              .getProvisioningJob(monitor);
+//          // updates cannot run from within Eclipse IDE!!!
+//          if (provisioningJob == null) {
+//            System.err
+//                .println("Running update from within Eclipse IDE? This won't work!!!");
+//            throw new NullPointerException();
+//          }
+//
+//          // register a job change listener to track
+//          // installation progress and notify user upon success
+//          provisioningJob
+//              .addJobChangeListener(new JobChangeAdapter() {
+//                @Override
+//                public void done(IJobChangeEvent event) {
+//                  if (event.getResult().isOK()) {
+//                    sync.syncExec(new Runnable() {
+//
+//                      @Override
+//                      public void run() {
+//                        boolean restart = MessageDialog
+//                            .openQuestion(parent,
+//                                "Updates installed, restart?",
+//                                "Updates have been installed successfully, do you want to restart?");
+//                        if (restart) {
+//                          workbench.restart();
+//                        }
+//                      }
+//                    });
+//
+//                  }
+//                  super.done(event);
+//                }
+//              });
+//
+//          provisioningJob.schedule();
+//        }
+//        return Status.OK_STATUS;
+//      }
+//    };
+//    j.schedule();
+//  }
+//
+//} 
