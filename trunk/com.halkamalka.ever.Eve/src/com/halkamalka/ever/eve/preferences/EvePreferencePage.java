28ac552fc10e21174a465d223d835a3d86b69cb8
buttonfly <buttonfly@87f88ac2-c770-b2fc-5759-3cc0efa11c14>
2014-06-01 07:31:52 +0000

    remove unused field from preference

--- trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/preferences/EvePreferencePage.java
+++ trunk/com.halkamalka.ever.Eve/src/com/halkamalka/ever/eve/preferences/EvePreferencePage.java
@@ -26,7 +26,7 @@ public class EvePreferencePage
 	public EvePreferencePage() {
 		super(GRID);
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
-		setDescription("A demonstration of a preference page implementation");
+//		setDescription("A demonstration of a preference page implementation");
 	}
 	
 	/**
@@ -36,24 +36,24 @@ public class EvePreferencePage
 	 * restore itself.
 	 */
 	public void createFieldEditors() {
-		addField(new DirectoryFieldEditor(PreferenceConstants.P_PATH, 
-				"&Directory preference:", getFieldEditorParent()));
-		addField(
-			new BooleanFieldEditor(
-				PreferenceConstants.P_BOOLEAN,
-				"&An example of a boolean preference",
-				getFieldEditorParent()));
-
-		addField(new RadioGroupFieldEditor(
-				PreferenceConstants.P_CHOICE,
-			"An example of a multiple-choice preference",
-			1,
-			new String[][] { { "&Choice 1", "choice1" }, {
-				"C&hoice 2", "choice2" }
-		}, getFieldEditorParent()));
-		addField(
-			new StringFieldEditor(PreferenceConstants.P_STRING, "A &text preference:", getFieldEditorParent()));
-		
+//		addField(new DirectoryFieldEditor(PreferenceConstants.P_PATH, 
+//				"&Directory preference:", getFieldEditorParent()));
+//		addField(
+//			new BooleanFieldEditor(
+//				PreferenceConstants.P_BOOLEAN,
+//				"&An example of a boolean preference",
+//				getFieldEditorParent()));
+//
+//		addField(new RadioGroupFieldEditor(
+//				PreferenceConstants.P_CHOICE,
+//			"An example of a multiple-choice preference",
+//			1,
+//			new String[][] { { "&Choice 1", "choice1" }, {
+//				"C&hoice 2", "choice2" }
+//		}, getFieldEditorParent()));
+//		addField(
+//			new StringFieldEditor(PreferenceConstants.P_STRING, "A &text preference:", getFieldEditorParent()));
+//		
 		addField(
 			new StringFieldEditor(PreferenceConstants.P_SERVER_IP, "A &Engine Server IP:", getFieldEditorParent()));
 		
